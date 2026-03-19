377f305f5463b0a120a1358abd88f1784ff0eb30
lalaina <tsikyloharantsoa@gmail.com>
2026-03-19 09:16:16 +0300

    fix react-helmet-async ESM interop for Vercel SSR

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { reactRouter } from '@react-router/dev/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter()],
+  ssr: {
+    noExternal: ['react-helmet-async']
+  }
 })
